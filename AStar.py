e58141a1838f7391c94fd045e32a46f644d750fe
Assel Al Assel <aseel1234500000@gmail.com>
2022-05-17 13:51:52 +0300

    end of flask and a star

---
+++ AStar.py
@@ -0,0 +1,220 @@
+##############################################################################
+
+# import packages
+
+##############################################################################
+import json
+
+
+import numpy as np
+
+import heapq
+
+import matplotlib.pyplot as plt
+
+from matplotlib.pyplot import figure
+
+
+##############################################################################
+
+# plot grid
+
+##############################################################################
+
+
+# grid = np.array([[2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+#                 [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0,
+#                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2],
+#                 [2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 2,
+#                     2, 0, 2, 2, 2, 2, 0, 2, 2, 0, 0, 2],
+#                 [2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2,
+#                     2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 2],
+#                 [2, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2,
+#                     2, 0, 2, 2, 2, 2, 2, 0, 2, 0, 0, 2],
+#                 [2, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0,
+#                     0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2],
+#                 [2, 0, 2, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0,
+#                     0, 2, 2, 2, 2, 0, 2, 0, 2, 0, 0, 2],
+#                 [2, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+#                     0, 0, 0, 0, 2, 0, 2, 2, 2, 0, 2, 2],
+#                 [2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+#                     0, 2, 2, 0, 2, 0, 0, 0, 0, 0, 0, 2],
+#                 [2, 0, 2, 2, 2, 2, 0, 2, 2, 2, 2, 0, 2,
+#                     0, 2, 2, 0, 2, 0, 2, 2, 2, 0, 2, 2],
+#                 [2, 0, 0, 0, 0, 2, 0, 2, 1, 1, 2, 0, 2,
+#                     0, 0, 0, 0, 2, 0, 2, 1, 2, 0, 0, 2],
+#                 [2, 0, 2, 2, 0, 2, 0, 2, 1, 1, 2, 0, 2,
+#                     0, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2],
+#                 [2, 0, 2, 2, 0, 2, 0, 2, 1, 1, 2, 0, 2,
+#                     0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+#                 [2, 0, 2, 2, 2, 2, 0, 2, 2, 2, 2, 0, 2,
+#                     0, 2, 0, 2, 2, 2, 0, 2, 2, 2, 0, 2],
+#                 [2, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 2,
+#                     2, 2, 0, 2, 0, 2, 0, 2, 1, 2, 0, 2],
+#                 [2, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0,
+#                     0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 0, 2],
+#                 [2, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2,
+#                     2, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2],
+#                 [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]])
+
+# print(grid)
+# start point and goal
+
+
+##############################################################################
+
+# heuristic function for path scoring
+
+##############################################################################
+
+
+def heuristic(a, b):
+
+    return np.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
+
+
+##############################################################################
+
+# path finding function
+
+##############################################################################
+
+
+def astar(array, start, goal):
+    array = np.array(array)
+    # neighbors = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
+    neighbors = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    close_set = set()
+
+    came_from = {}
+
+    gscore = {start: 0}
+
+    fscore = {start: heuristic(start, goal)}
+
+    oheap = []
+
+    heapq.heappush(oheap, (fscore[start], start))
+
+    while oheap:
+
+        current = heapq.heappop(oheap)[1]
+
+        if current == goal:
+
+            data = []
+
+            while current in came_from:
+
+                data.append(current)
+
+                current = came_from[current]
+
+            return data
+
+        close_set.add(current)
+
+        for i, j in neighbors:
+
+            neighbor = current[0] + i, current[1] + j
+
+            tentative_g_score = gscore[current] + heuristic(current, neighbor)
+
+            if 0 <= neighbor[0] < array.shape[0]:
+
+                if 0 <= neighbor[1] < array.shape[1]:
+
+                    if array[neighbor[0]][neighbor[1]] == 2:
+
+                        continue
+
+                else:
+
+                    # array bound y walls
+
+                    continue
+
+            else:
+
+                # array bound x walls
+
+                continue
+
+            if neighbor in close_set and tentative_g_score >= gscore.get(neighbor, 0):
+
+                continue
+
+            if tentative_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1]for i in oheap]:
+
+                came_from[neighbor] = current
+
+                gscore[neighbor] = tentative_g_score
+
+                fscore[neighbor] = tentative_g_score + \
+                    heuristic(neighbor, goal)
+
+                heapq.heappush(oheap, (fscore[neighbor], neighbor))
+
+    return False
+
+
+start = (1, 1)
+
+goal = (1, 15)
+
+
+def startGame(game):
+
+    route = astar(game, start, goal)
+
+    route = route + [start]
+
+    route = route[::-1]
+
+    return (list(route))
+
+    # data = {
+    #     # "maze":list(grid),
+    #     "solution": list(route)
+    # }
+    # json_string = json.dumps(data)
+    # return json_strin
+
+
+##############################################################################
+
+# plot the path
+
+##############################################################################
+
+
+# extract x and y coordinates from route list
+
+# x_coords = []
+
+# y_coords = []
+
+# for i in (range(0,len(route))):
+
+#     x = route[i][0]
+
+#     y = route[i][1]
+
+#     x_coords.append(x)
+
+#     y_coords.append(y)
+
+# # plot map and path
+
+# fig, ax = plt.subplots(figsize=(20,20))
+
+# ax.imshow(grid, cmap=plt.cm.Dark2)
+
+# ax.scatter(start[1],start[0], marker = "*", color = "yellow", s = 200)
+
+# ax.scatter(goal[1],goal[0], marker = "*", color = "red", s = 200)
+
+# ax.plot(y_coords,x_coords, color = "black")
+
+# plt.show()
--- flasktest.py
+++ flasktest.py
@@ -1,17 +1,23 @@
 from cgitb import reset
-from flask import Flask,render_template,request,Response
+import json
+from flask import Flask, render_template, request, Response
+from AStar import startGame
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/solve",methods=['POST'])
-def solve():
-   test= request.get_json("arr")
-   return render_template("index.html",arr=test)
 
-    
+@app.route("/solve", methods=['POST'])
+def solve():
+    game = request.form['game']
+    gamePlay = json.loads(game)
+    sol = startGame(gamePlay)
+    return Response(json.dumps(sol), mimetype='application/json')
+
+
 if __name__ == "__main__":
-     app.run()
+    app.run(host='0.0.0.0')

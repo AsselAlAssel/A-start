b5b4fdf6e472bdb7b5826e81a20bb509985df5f2
Huthaifa-Dev <huthaifajamal2@gmail.com>
2022-05-21 14:09:49 +0300

    working car with delayed steps

--- static/index.js
+++ static/index.js
@@ -1,4 +1,5 @@
-var table = document.querySelector('#game-container'), rowIndex, colIndex;
+const table = document.querySelector('#grid-container');
+const car = document.querySelector('#car');
 var start, finish = 0;
 const game = [
     [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
@@ -19,36 +20,60 @@ const game = [
     [2, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 0, 2],
     [2, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2],
     [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]];
+
+// const tempGame = game;
 const solve = () => {
 
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function () {
         const z = this.responseText;
         console.log(z)
-        console.log(JSON.parse(z));
+        // console.log(JSON.parse(z));
+        const solution = JSON.parse(z);
+        printPath(solution);
     }
     xhttp.open("POST", "/solve");
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send(`game=${JSON.stringify(game)}`);
 }
 
-const Generate = () => {
 
-    for (let i = 0; i < game.length; i++) {
-        for (let j = 1; j < game[i].length - 1; j++) {
-            if (i === 0 || i === game.length - 1 || j === 0 || j === game[i].length - 1) {
-                game[i][j] = 2;
-                continue;
-            }
-            let current = Math.random() * 1;
-            if (current < 0.4) {
-                game[i][j] = 2;
-                continue;
-            }
-            game[i][j] = 0;
+const timer = ms => new Promise(res => setTimeout(res, ms))
+const printPath = async solution => {
+    console.log(solution);
+    car.classList.remove('hidden');
+    for (index in solution) {
 
-        }
+        console.log(index);
+
+        const yIndex = solution[index][0] * 32;
+        const xIndex = solution[index][1] * 32;
+        car.style.left = xIndex + 'px';
+        car.style.top = yIndex + 'px';
+        console.log(xIndex, yIndex);
+        await timer(1000);
     }
+}
+
+const Generate = () => {
+    // console.log(tempGame);
+
+    // for (let i = 0; i < game.length; i++) {
+    //     for (let j = 1; j < game[i].length - 1; j++) {
+    //         if (i === 0 || i === game.length - 1 || j === 0 || j === game[i].length - 1) {
+    //             game[i][j] = 2;
+    //             continue;
+    //         }
+    //         let current = Math.random() * 1;
+    //         if (current < 0.4) {
+    //             game[i][j] = 2;
+    //             continue;
+    //         }
+    //         game[i][j] = 0;
+
+    //     }
+    // }
+    console.log(game);
     table.innerHTML = '';
     game.map(row => {
         let table_row = document.createElement("tr");
